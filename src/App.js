4bbe14b8dcf00bb4734264ade0942a91e7f72c71
Arely_SU <al060223@uacam.mx>
2023-11-29 00:31:58 -0600

    cuadro de texto2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         Insert URL or type prompt:
       </p>
       <label htmlFor="imagen-url"> </label>
-      <input id="imagen-url" type="text" name="imagen" />
+      <input id="imagen-url" type="text" name="imagen" placeholder="introduzca la url a analizar o un texto para generar una imagen" />
       <button type="button">Analizar</button>
       <button type="button">Generar</button>
     </div>
