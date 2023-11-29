94587e42912219401556c69e269300d71cf4a20d
Arely_SU <al060223@uacam.mx>
2023-11-29 00:49:54 -0600

    cambios en app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>
+        Computer vision
+      </h1>
+      <p>
+        Insert URL or type prompt:
+      </p>
+      <label htmlFor="imagen-url"> </label>
+      <input id="imagen-url" type="text" name="imagen" placeholder="introduzca la url a analizar o un texto para generar una imagen" />
+      <br/>
+      <button type="button">Analizar</button>
+      <button type="button">Generar</button>
     </div>
+ 
   );
 }
 
