08838d1a2b6a8168e2625e52c9e0d360c0027d30
Arely_SU <al060223@uacam.mx>
2023-11-28 23:52:50 -0600

    Contenedor y caja de texto

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import React from 'react';
 function App() {
   //tirulo de la pagina
   return (
-    <div className="miApp">
-      <h1>Computer vision</h1>
-      <p>My first Insert URL or type prompt!</p>
+    <div className="app-container">
+      <h1>
+        Computer vision
+      </h1>
+      <p>
+        Insert URL or type prompt:
+      </p>
+      <label htmlFor="imagen-url"> URL de la imagen:</label>
     </div>
   );
 }
