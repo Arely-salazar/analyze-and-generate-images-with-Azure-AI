4003e7c85f52e6d8a1addbe6ab16e343c83f529f
Arely_SU <al060223@uacam.mx>
2023-11-29 01:13:32 -0600

    creacion de azure-image-analysis.js

---
+++ my-app/src/azure-image-analysis.js
@@ -0,0 +1,27 @@
+// azure-image-analysis.js
+import axios from 'axios';
+
+const analyzeImage = async (imageUrl, features) => {
+  try {
+    const response = await axios.post(
+      'https://<nombre-recurso>.cognitiveservices.azure.com/vision/v4.0/analyze',
+      {
+        url: imageUrl,
+        visualFeatures: features || 'Categories,Description,Color',
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Ocp-Apim-Subscription-Key': process.env.REACT_APP_AZURE_VISION_KEY,
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error('Error analyzing image:', error);
+    throw error;
+  }
+};
+
+export default analyzeImage;
